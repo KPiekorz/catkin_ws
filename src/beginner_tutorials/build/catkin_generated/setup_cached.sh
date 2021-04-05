705313bb917cd26463e9c06b5875983e60239b06
Krzysztof <kpiekorz919@protonmail.com>
2021-04-05 21:33:56 +0200

    init commit

---
+++ src/beginner_tutorials/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/kpiekorz/catkin_ws/src/beginner_tutorials/build/devel:$CMAKE_PREFIX_PATH"
+export PKG_CONFIG_PATH="/home/kpiekorz/catkin_ws/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
+export PYTHONPATH="/home/kpiekorz/catkin_ws/devel/lib/python2.7/dist-packages:$PYTHONPATH"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/kpiekorz/catkin_ws/src/beginner_tutorials/build/devel/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
+export ROS_PACKAGE_PATH="/home/kpiekorz/catkin_ws/src/beginner_tutorials:$ROS_PACKAGE_PATH"
